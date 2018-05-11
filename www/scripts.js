d76f45a1c79ccb642aceec70098836c5fc328ca9
Darren MacDonald <darrentmacdonald@gmail.com>
2018-05-11 02:07:23 -0400

    Adding Angular v6

--- www/scripts.js
+++ www/scripts.js
@@ -32,15 +32,9 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
 
 app.initialize();
+;
+//# sourceMappingURL=scripts.js.map
