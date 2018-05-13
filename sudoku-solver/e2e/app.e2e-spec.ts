e09df1ae120503ff60aeb3c876c0812e05209219
Darren MacDonald <darrentmacdonald@gmail.com>
2018-05-13 01:34:51 -0400

    Converting to Eclipse IDE project. Game grid is em size based

--- sudoku-solver/e2e/app.e2e-spec.ts
+++ sudoku-solver/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('sudoku-solver App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- sudoku-solver/src/app/app.component.ts
+++ sudoku-solver/src/app/app.component.ts
@@ -9,5 +9,5 @@ export class AppComponent {
   title = 'Sudoku Solver';
   data = [0,0,0,0,0,0,0,0,0]; // row-major ordering of 9
   
-  tapCell(index : int) { this.data[index]++; }
+  tapCell(index : number) { this.data[index]++; }
 }
